f690aa0bea239063771f5b2d5d98a43059680bd7
mlaskowski7 <mtlaskowski7@gmail.com>
2025-03-27 18:34:04 +0100

    refactor: separate AddUser method in UserService.cs

--- tutorial3/LegacyApp/UserService.cs
+++ tutorial3/LegacyApp/UserService.cs
@@ -1,23 +1,29 @@
 ﻿using System;
+using LegacyApp.DataAccessLayer;
 using LegacyApp.DateTimeProviders;
 using LegacyApp.Exceptions;
+using LegacyApp.Models;
 
 namespace LegacyApp
 {
     public class UserService
     {
-        private readonly IDateTimeProvider _dateTime;
+        private readonly IDateTimeProvider _dateTimeProvider;
         
         private readonly IClientRepository _clientRepository;
+        
+        private readonly IUserCreditService _userCreditService;
 
-        public UserService() : this(new StdDateTimeProvider(), new ClientRepository())
+        public UserService() 
+            : this(new StdDateTimeProvider(), new ClientRepository(), new UserCreditService())
         {
         }
 
-        public UserService(IDateTimeProvider dateTime, IClientRepository clientRepository)
+        public UserService(IDateTimeProvider dateTimeProvider, IClientRepository clientRepository, IUserCreditService userCreditService)
         {
-            _dateTime = dateTime;
+            _dateTimeProvider = dateTimeProvider;
             _clientRepository = clientRepository;
+            _userCreditService = userCreditService;
         }
 
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
@@ -33,29 +39,12 @@ namespace LegacyApp
                 return false;
             }
             
-            if (user.Client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (user.Client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            UpdateCreditLimitBasedOnUsersClientType(user);
+            return AddUserIfEnoughCreditLimit(user);
+        }
 
+        private bool AddUserIfEnoughCreditLimit(User user)
+        {
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
                 return false;
@@ -63,6 +52,37 @@ namespace LegacyApp
 
             UserDataAccess.AddUser(user);
             return true;
+        }
+
+        private void UpdateCreditLimitBasedOnUsersClientType(User user)
+        {
+            var clientType = user.Client.Type;
+
+            if (clientType == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else
+            {
+                var creditLimit = GetCreditLimitForUser(user);
+                user.HasCreditLimit = true;
+                user.CreditLimit = clientType == "ImportantClient" ? 
+                                                    creditLimit * 2 : 
+                                                    creditLimit;
+            }
+        }
+
+        private int GetCreditLimitForUser(User user)
+        {
+            try
+            {
+                var creditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                return creditLimit;
+            }
+            catch (ArgumentException argumentException)
+            {
+                throw new UserCreditNotFoundException("Could not find user's credit limit by his last name", argumentException);
+            }
         }
 
         private User TryCreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
@@ -101,13 +121,13 @@ namespace LegacyApp
                 throw new UserValidationException($"Invalid email address was passed ({email}).");
             }
             
-            var now = _dateTime.Now;
+            var now = _dateTimeProvider.Now;
             int age = now.Year - dateOfBirth.Year;
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
 
             if (age < 21)
             {
-                throw new UserValidationException("Age of the user must be under 21.");
+                throw new UserValidationException("Age of the user must be over 21.");
             }
         }
     }
