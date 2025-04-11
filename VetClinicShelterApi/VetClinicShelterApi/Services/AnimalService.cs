a8e45bafafc2b0a0661b9daf1071c423812c77f0
mlaskowski7 <mtlaskowski7@gmail.com>
2025-04-11 22:51:12 +0200

    feat: impl data access layer for animals

---
+++ VetClinicShelterApi/VetClinicShelterApi/Services/AnimalService.cs
@@ -0,0 +1,34 @@
+﻿using VetClinicShelterApi.Dtos.Request;
+using VetClinicShelterApi.Dtos.Response;
+using VetClinicShelterApi.Utils;
+
+namespace VetClinicShelterApi.Services
+{
+    public class AnimalService : IAnimalService
+    {
+        public AnimalResponseDto CreateAnimal(AnimalRequestDto animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAnimalById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<AnimalResponseDto> GetAllAnimals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWrapper<AnimalResponseDto> GetAnimalById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWrapper<AnimalResponseDto> UpdateAnimal(Guid id, AnimalRequestDto animal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
